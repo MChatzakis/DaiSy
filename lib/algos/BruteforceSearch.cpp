d7e0f2b3571424c6715722175e4ddcda37d49ccb
gaia <gayathiri.ravendirane@gmail.com>
2025-05-19 16:55:44 +0200

    changed loadBinData to loadRandomData

--- lib/algos/BruteforceSearch.cpp
+++ lib/algos/BruteforceSearch.cpp
@@ -41,6 +41,7 @@ namespace diNoLib
         this->n_database = n_database;
         this->dim = dim;
     }
+    
     void BruteForceSearch::searchIndex(const float *query, const idx_t n_query, const idx_t k, idx_t *I, float *D)
     {
         #pragma omp parallel num_threads(num_threads)
