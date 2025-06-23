32070ce27babdde98fb7c9e53d478980856b8f2d
gaia <gayathiri.ravendirane@gmail.com>
2025-06-23 11:31:51 +0200

    debugging

--- lib/algos/Messi.hpp
+++ lib/algos/Messi.hpp
@@ -73,8 +73,8 @@ namespace diNoLib
         int min_checked_leaves = -1;
         
         int read_block_length = 100000;
-        int search_workers = 64;
-        int index_workers = 32;
+        int search_workers = 2; //64;
+        int index_workers = 2; //32;
         int n_pqueue = 42;
         int warping_window = 5;
 
