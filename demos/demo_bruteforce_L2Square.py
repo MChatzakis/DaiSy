57a2a746ad9b94251ac17c0240b0a4357cb21621
FrancescaDelGaudio <francescadelgaudio56@gmail.com>
2025-12-11 15:41:05 +0100

    feat: gitignore + pybinds

--- demos/demo_bruteforce_L2Square.py
+++ demos/demo_bruteforce_L2Square.py
@@ -4,7 +4,7 @@ import numpy as np
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from scripts.gui_config_param import get_config, search_classes
+#from scripts.gui_config_param import get_config, search_classes
 
 from diNoSimilaritySearch import DistanceType, BruteForceSearch
 
